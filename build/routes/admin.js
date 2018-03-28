8d39a12fd3d833c79c57b318097d3789d9ada8a0
hale <hale.huo@zhangmen.com>
2018-03-28 13:41:50 +0800

    后台查看最近访问10条

--- build/routes/admin.js
+++ build/routes/admin.js
@@ -42,9 +42,10 @@ var Routes = (function () {
                 blog_model_1.default.aggregate({ $group: { _id: null, pvCount: { $sum: '$pv' } } }),
                 viewer_log_model_1.default.count({ createdAt: { $regex: today, $options: 'i' } }),
                 viewer_log_model_1.default.aggregate({ $match: { createdAt: { $regex: today, $options: 'i' } } }, { $group: { _id: { blogId: '$blogId', title: "$title" }, pv: { $sum: 1 } } }, { $sort: { createAt: -1 } }),
+                viewer_log_model_1.default.find({}, null, { sort: { _id: -1 }, limit: 10 })
             ]).then(function (_a) {
-                var result1 = _a[0], result2 = _a[1], result3 = _a[2];
-                return { readCount: result1[0].pvCount, todayRead: result2, recent: result3 };
+                var result1 = _a[0], result2 = _a[1], result3 = _a[2], result4 = _a[3];
+                return { readCount: result1[0].pvCount, todayRead: result2, recent: result3, newRead: result4 };
             });
         }
         else {
