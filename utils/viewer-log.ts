57b16131ed672f3d9f4dc56781480bc3df3c444a
hale <hale.huo@zhangmen.com>
2018-04-12 10:04:31 +0800

    访问日志

--- utils/viewer-log.ts
+++ utils/viewer-log.ts
@@ -10,29 +10,36 @@ import BlogModel from '../models/blog-model';
 export default function (req: Request) {
     var ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var realIp = req.headers['x-forwarded-for'];
-    if (req.headers['referer'] && req.headers['user-agent']) {
-        if (realIp) {
-            realIp = realIp.substring(0, realIp.indexOf(','))
-        } else {
-            realIp = ip;
-        }
-        if (req.originalUrl.lastIndexOf("blogdetail/") > 0) {
-            const blogId = req.originalUrl.substring(req.originalUrl.lastIndexOf("/") + 1);
-            BlogModel.findById(blogId)
-                .then(blog => {
-                    var pvLogObj = new ViewerLogModel({
-                        ip: realIp,
-                        blogId,
-                        title: blog.title,
-                        url: req.originalUrl,
-                        referer: req.headers['referer'] || '',
-                        userAgent: req.headers['user-agent'] || '',
-                        createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-                    });
-                    return pvLogObj.save();
-                }).then(() => {
-                    console.log("访问记录成功！");
-                })
-        }
+    if (realIp) {
+        realIp = realIp.substring(0, realIp.indexOf(','))
+    } else {
+        realIp = ip;
+    }
+    if (req.originalUrl.lastIndexOf("blogdetail/") > 0) {
+        const blogId = req.originalUrl.substring(req.originalUrl.lastIndexOf("/") + 1);
+        BlogModel.findById(blogId)
+            .then(blog => {
+                var pvLogObj = new ViewerLogModel({
+                    ip: realIp,
+                    blogId,
+                    title: blog.title,
+                    url: req.originalUrl,
+                    referer: req.headers['referer'] || '',
+                    userAgent: req.headers['user-agent'] || '',
+                    createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+                });
+                return pvLogObj.save();
+            }).then(() => {
+                console.log("访问记录成功！");
+            })
+    } else {
+        var pvLogObj = new ViewerLogModel({
+            ip: realIp,
+            url: req.originalUrl,
+            referer: req.headers['referer'] || '',
+            userAgent: req.headers['user-agent'] || '',
+            createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+        });
+        return pvLogObj.save();
     }
 }
