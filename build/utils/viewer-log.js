09817fa949eb23146b05b3468d2fefb74471dabd
hale <hale.huo@zhangmen.com>
2018-06-20 13:43:58 +0800

    只记录有refer的访问

--- build/utils/viewer-log.js
+++ build/utils/viewer-log.js
@@ -6,42 +6,44 @@ var blog_model_1 = require("../models/blog-model");
 function default_1(req) {
     var ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var realIp = req.headers['x-forwarded-for'];
-    if (realIp) {
-        realIp = realIp.substring(0, realIp.indexOf(','));
-    }
-    else {
-        realIp = ip;
-    }
-    if (req.originalUrl.lastIndexOf("blogdetail/") > 0) {
-        var blogId_1 = req.originalUrl.substring(req.originalUrl.lastIndexOf("/") + 1);
-        blog_model_1.default.findById(blogId_1)
-            .then(function (blog) {
+    if (req.headers['referer'] && req.headers['user-agent']) {
+        if (realIp) {
+            realIp = realIp.substring(0, realIp.indexOf(','));
+        }
+        else {
+            realIp = ip;
+        }
+        if (/\/blogdetail/.test(req.originalUrl)) {
+            var blogId_1 = req.originalUrl.substring(req.originalUrl.lastIndexOf("/") + 1);
+            blog_model_1.default.findById(blogId_1)
+                .then(function (blog) {
+                var pvLogObj = new viewer_log_model_1.default({
+                    ip: realIp,
+                    blogId: blogId_1,
+                    title: blog.title,
+                    url: req.originalUrl,
+                    referer: req.headers['referer'] || '',
+                    userAgent: req.headers['user-agent'] || '',
+                    createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
+                });
+                return pvLogObj.save();
+            }).then(function () {
+                console.log("访问记录成功！");
+            });
+        }
+        else if (/\/catalog/.test(req.originalUrl)
+            || /\/quicknote/.test(req.originalUrl)
+            || /\/weibo/.test(req.originalUrl)
+            || /\/about/.test(req.originalUrl)) {
             var pvLogObj = new viewer_log_model_1.default({
                 ip: realIp,
-                blogId: blogId_1,
-                title: blog.title,
                 url: req.originalUrl,
                 referer: req.headers['referer'] || '',
                 userAgent: req.headers['user-agent'] || '',
                 createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
             });
             return pvLogObj.save();
-        }).then(function () {
-            console.log("访问记录成功！");
-        });
-    }
-    else if (req.originalUrl.indexOf("/catalog") > -1
-        || req.originalUrl.indexOf("/quicknote") > -1
-        || req.originalUrl.indexOf("/weibo") > -1
-        || req.originalUrl.indexOf("/about") > -1) {
-        var pvLogObj = new viewer_log_model_1.default({
-            ip: realIp,
-            url: req.originalUrl,
-            referer: req.headers['referer'] || '',
-            userAgent: req.headers['user-agent'] || '',
-            createdAt: moment().format('YYYY-MM-DD HH:mm:ss'),
-        });
-        return pvLogObj.save();
+        }
     }
 }
 exports.default = default_1;
